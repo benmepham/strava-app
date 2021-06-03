1b80cd45a36231bbc705c03aa0d05abd6471adab
Ben Mepham <25695617+benmepham@users.noreply.github.com>
2021-06-03 11:25:15 +0100

    improved run api error handling

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,7 @@ exports.view = async function (req, res) {
         req.user.refresh_token,
         req.user.expires_at
     );
+    console.log(returned_access_token);
     let runs = await runFetch.getRuns(
         returned_access_token,
         req.query.page,
@@ -19,6 +20,9 @@ exports.view = async function (req, res) {
     let runArray = [];
     for (let run of runs.runs) {
         run = await runFetch.getActivityData(run.id, returned_access_token);
+        if (run.status != 200) {
+            return res.status(run.status).send();
+        }
         run = run.data;
         runArray.push(runFetch.parseRun(run));
     }
