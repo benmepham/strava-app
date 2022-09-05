46629560b8d73b74a1de97c9521b48d38507d734
Ben Mepham <25695617+benmepham@users.noreply.github.com>
2022-09-05 10:42:35 +0100

    renaming

--- util/email.js
+++ util/email.js
@@ -17,7 +17,7 @@ setup().catch(console.error);
 
 async function sendMail(to, subject, text, html) {
     let info = await transporter.sendMail({
-        from: '"5K App" <'+process.env.EMAIL_USER+'>',
+        from: '"Strava App" <'+process.env.EMAIL_USER+'>',
         to: to,
         subject: subject,
         text: text,
