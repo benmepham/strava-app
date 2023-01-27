32b7a69100a63d0970f08f798ebcae55cf4bd9d2
Ben Mepham <25695617+benmepham@users.noreply.github.com>
2023-01-27 23:57:15 +0000

    fix run validation

--- util/validateRun.js
+++ util/validateRun.js
@@ -1,3 +1,8 @@
-validateRun = (run) => run.type == "Run" && run.distance >= 5000 && !run.manual;
+validateRun = (run) =>
+    run.type == "Run" &&
+    run.distance >= 5000 &&
+    !run.manual &&
+    run.start_latlng &&
+    run.start_latlng.length != 0;
 
 module.exports = { validateRun };
